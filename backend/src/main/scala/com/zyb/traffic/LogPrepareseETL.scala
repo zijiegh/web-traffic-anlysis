c7ff9714cbefaf7c268acf16870450332d4f3ed0
zijie <2503241540@qq.com>
2019-05-14 13:58:52 +0800

    complete log data preparsed processing.

--- backend/src/main/scala/com/zyb/traffic/LogPrepareseETL.scala
+++ backend/src/main/scala/com/zyb/traffic/LogPrepareseETL.scala
@@ -6,14 +6,22 @@ import org.apache.spark.sql.{Encoders, SaveMode, SparkSession}
 object LogPrepareseETL {
   def main(args: Array[String]): Unit = {
 
+    val conf=new SparkConf().setAppName("LogPrepareseETL")
+    if(!conf.contains("spark.master")){
+      conf.setMaster("local")
+    }
+
     val spark=SparkSession.builder()
-      .appName("LogPrepareseETL")
-      .master("local[3]")
+      .config(conf)
       .enableHiveSupport()
       .getOrCreate()
 
+    val inPath=conf.get("spark.traffic.rawdata.inPath",
+      "file:///F:\\zhangyingbiao\\ProjectDome\\imitation\\web-traffic-combat\\backend\\src\\main\\resources\\web-log\\*")
+    val savedTable=if(args.length>=1) args(0) else "traffic.rawlog"
+
     //在指定default-fs为hdfs时(core-site.xml),读取本地文件使用file://协议
-    val rawLine=spark.sparkContext.textFile("file:///F:\\zhangyingbiao\\ProjectDome\\imitation\\web-traffic-anlysis\\backend\\src\\main\\resources\\web-log\\*",3)
+    val rawLine=spark.sparkContext.textFile(inPath,3)
 
     val parsedLog=rawLine.flatMap(l=>Option(LogPreparser.parse(l)))
 
@@ -21,7 +29,7 @@ object LogPrepareseETL {
       .repartition(3)
       .write.partitionBy("year","month","day")
       .mode(SaveMode.Overwrite)
-      .saveAsTable("traffic.rawlog")
+      .saveAsTable(savedTable)
 
     spark.stop()
 
